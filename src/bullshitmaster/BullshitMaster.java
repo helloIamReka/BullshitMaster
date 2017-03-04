e4986bfbb726b7a2e82f51ddcad5a880d6a76b4a
helloIamReka <reka.korosy@gmail.com>
2017-03-04 20:06:24 +0100

    Update BullshitMaster.java

--- src/bullshitmaster/BullshitMaster.java
+++ src/bullshitmaster/BullshitMaster.java
@@ -1,6 +1,6 @@
 
 package bullshitmaster;
-/**
+/** This program reads in a text that the user can paste or type, calculates the bullshit %, and gives a career prediction based on it.
  *
  * @author R
  */
